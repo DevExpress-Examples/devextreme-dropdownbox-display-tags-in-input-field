bf5a8763fd215a0b7e0861a1d5237f28b0ad9981
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-05 17:15:05 +0300

    React - Add DevExtreme licensing

--- React/src/main.tsx
+++ React/src/main.tsx
@@ -1,3 +1,8 @@
+import config from 'devextreme/core/config';
+import { licenseKey } from './devextreme-license';
+
+config({ licenseKey });
+
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
